3fe541955f8b0a517af88160b93f3d51dcfc562d
Jihwan Lee <wlghks0508@naver.com>
2025-07-15 15:13:04 +0900

    file 생성

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
 // var balanceRouter = require("./routes/real");
 var userStockRouter = require("./routes/userStock");
+var myPageRouter = require("./routes/myPage");
 /* --------------------------------------- */
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -51,6 +52,7 @@ app.use("/", indexRouter);
 app.use("/auth", authRouter);
 // app.use("/api/real", balanceRouter);
 app.use("/api/userStock", userStockRouter);
+app.use("/api/mypage", myPageRouter);
 /* --------------------------------------- */
 const port = process.env.PORT || 3001;
 
