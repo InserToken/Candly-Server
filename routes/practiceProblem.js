0a6b1d82fcd60355ff82b60d9c38eeb73ef8d02c
EunseoJun <jeuns1108@gmail.com>
2025-07-18 14:04:57 +0900

    MA:날짜 수정

--- routes/practiceProblem.js
+++ routes/practiceProblem.js
@@ -46,7 +46,7 @@ router.get("/:problemId", async (req, res) => {
     }
 
     // 19일 전부터 20일 후까지 자르기
-    const start = Math.max(0, targetIndex - 19);
+    const start = Math.max(0, targetIndex - 139);
     const end = Math.min(sortedPrices.length, targetIndex + 21);
     const tenDaySlice = sortedPrices.slice(start, end);
 
